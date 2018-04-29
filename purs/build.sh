7edd0e2739524cf3eb1cdcd144befa867ac39b49
Aiden McClelland <amcclelland@saltlending.com>
2018-04-29 10:36:21 -0600

    ditching purescript

--- purs/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pulp build && pulp test && purs-tsd-gen -d "$DIR/output/" Main
--- purs/src/Secure.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var crypto = require('crypto');
-
-exports.randomBytes = function(size) {
-  return function() {
-    return crypto.randomBytes(size);
-  };
-};
