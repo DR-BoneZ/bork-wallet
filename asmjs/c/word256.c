a407474718bd1c1686fa3bc1763f1c39591b28fc
Aiden McClelland <amcclelland@saltlending.com>
2018-05-04 14:12:46 -0600

    rm node_modules

--- asmjs/c/word256.c
+++ asmjs/c/word256.c
@@ -1,80 +1,64 @@
 #include <string.h>
 #include "word256.h"
 
-void shl256(word256* word, uint32_t by) {
+void shl256(word256 word, uint32_t by) {
     word256 out;
-    if (by <= 64) {
-        out.w3 = (word->w3 << by);
-        out.w2 = (word->w2 << by) + (word->w3 >> (64 - by));
-        out.w1 = (word->w1 << by) + (word->w2 >> (64 - by));
-        out.w0 = (word->w0 << by) + (word->w1 >> (64 - by));
-    } else if (by <= 128) {
-        out.w3 = 0;
-        out.w2 = (word->w3 << (by % 64));
-        out.w1 = (word->w2 << (by % 64)) + (word->w3 >> (64 - (by % 64)));
-        out.w0 = (word->w1 << (by % 64)) + (word->w2 >> (64 - (by % 64)));
-    } else if (by <= 192) {
-        out.w3 = 0;
-        out.w2 = 0;
-        out.w1 = (word->w3 << (by % 64));
-        out.w0 = (word->w2 << (by % 64)) + (word->w3 >> (64 - (by % 64)));
-    } else {
-        out.w3 = 0;
-        out.w2 = 0;
-        out.w1 = 0;
-        out.w0 = (word->w3 << (by % 64));
+    int shlb = by % 8;
+    int shlB = by / 8;
+    int shrb = 8 - shlb;
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        if (i < shlB) {
+            out[WORD256_BYTES-1-i] = 0;
+        } else if (i == shlB) {
+            out[WORD256_BYTES-1-i] = word[WORD256_BYTES-1] << shlb;
+        } else {
+            out[WORD256_BYTES-1-i] = (word[WORD256_BYTES-1-i+shlB] << shlb) + (word[WORD256_BYTES-i+shlB] >> shrb);
+        }
     }
-    memcpy(word, &out, sizeof(word256));
+    memcpy(word, out, sizeof(word256));
 }
 
-void shr256(word256* word, uint32_t by) {
+void shr256(word256 word, uint32_t by) {
     word256 out;
-    out.w0 = word->w0 >> by;
-    out.w1 = word->w1 >> by;
-    out.w2 = word->w2 >> by;
-    out.w3 = word->w3 >> by;
-    if (by <= 64) {
-        out.w0 += word->w0 << (64 - by);
-        out.w1 += word->w1 << (64 - by);
-        out.w2 += word->w2 << (64 - by);
-        out.w3 += word->w3 << (64 - by);
-    } else if (by <= 128) {
-        out.w0 += word->w1 << (128 - by);
-        out.w1 += word->w2 << (128 - by);
-        out.w2 += word->w3 << (128 - by);
-    } else if (by <= 192) {
-        out.w0 += word->w2 << (192 - by);
-        out.w1 += word->w3 << (192 - by);
-    } else {
-        out.w0 += word->w3 << (256 - by);
+    int shrb = by % 8;
+    int shrB = by / 8;
+    int shlb = 8 - shrb;
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        if (i < shrB) {
+            out[i] = 0;
+        } else if (i == shrB) {
+            out[i] = word[0] >> shrb;
+        } else {
+            out[i] = (word[i-shrB] >> shrb) + (word[i-shrB-1] << shlb);
+        }
     }
-    memcpy(word, &out, sizeof(word256));
+    memcpy(word, out, sizeof(word256));
 }
 
-void add256(word256 in1, word256 in2, word256* out) {
-    out->w0 = in1.w0 + in2.w0;
-    out->w1 = in1.w1 + in2.w1 + (in1.w0 + in2.w0 < in1.w0);
-    out->w2 = in1.w2 + in2.w2 + (in1.w1 + in2.w1 < in1.w1);
-    out->w3 = in1.w3 + in2.w3 + (in1.w2 + in2.w2 < in1.w2);
+void add256(word256 in1, word256 in2, word256 out) {
+    int carry = 0;
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        out[i] = in1[i] + in2[i] + carry;
+        carry = out[i] < in1[i] + in2[i];
+    }
 }
 
-void sub256(word256 in1, word256 in2, word256* out) {
-    out->w0 = in1.w0 - in2.w0;
-    out->w1 = in1.w1 - in2.w1 - (in1.w0 - in2.w0 > in1.w0);
-    out->w2 = in1.w2 - in2.w2 - (in1.w1 - in2.w1 > in1.w1);
-    out->w3 = in1.w3 - in2.w3 - (in1.w2 + in2.w2 < in1.w2);
+void sub256(word256 in1, word256 in2, word256 out) {
+    int carry = 0;
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        out[i] = in1[i] - in2[i] - carry;
+        carry = out[i] > in1[i] - in2[i];
+    }
 }
 
-void and256(word256 in1, word256 in2, word256* out) {
-    out->w0 = in1.w0 & in2.w0;
-    out->w1 = in1.w1 & in2.w1;
-    out->w2 = in1.w2 & in2.w2;
-    out->w3 = in1.w3 & in2.w3;
+void and256(word256 in1, word256 in2, word256 out) {
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        out[i] = in1[i] & in2[i];
+    }
 }
 
-void or256(word256 in1, word256 in2, word256* out) {
-    out->w0 = in1.w0 | in2.w0;
-    out->w1 = in1.w1 | in2.w1;
-    out->w2 = in1.w2 | in2.w2;
-    out->w3 = in1.w3 | in2.w3;
+void or256(word256 in1, word256 in2, word256 out) {
+    for (int i = 0; i < WORD256_BYTES; i++) {
+        out[i] = in1[i] | in2[i];
+    }
 }
--- asmjs/c/word256.h
+++ asmjs/c/word256.h
@@ -1,22 +1,16 @@
 #include <stdint.h>
 
-struct word256 {
-    uint64_t w0;
-    uint64_t w1;
-    uint64_t w2;
-    uint64_t w3;
-};
+#define WORD256_BYTES 32
+typedef uint8_t word256[WORD256_BYTES];
 
-typedef struct word256 word256;
+void shl256(word256, uint32_t);
 
-void shl256(word256*, uint32_t);
+void shr256(word256, uint32_t);
 
-void shr256(word256*, uint32_t);
+void add256(word256, word256, word256);
 
-void add256(word256, word256, word256*);
+void sub256(word256, word256, word256);
 
-void sub256(word256, word256, word256*);
+void and256(word256, word256, word256);
 
-void and256(word256, word256, word256*);
-
-void or256(word256, word256, word256*);
+void or256(word256, word256, word256);
