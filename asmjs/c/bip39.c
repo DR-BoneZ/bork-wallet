97c65293f2263a47970056c3567accff50109abc
Aiden McClelland <amcclelland@saltlending.com>
2018-05-07 09:05:12 -0600

    bip39 complete

--- asmjs/c/bip39.c
+++ asmjs/c/bip39.c
@@ -1,17 +1,34 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include "word256.h"
 #include "sha2.h"
+#include "bip39.h"
 
-//#define BYTE_ORDER LITTLE_ENDIAN
-
-int entropyToSeed(word256 entropy) {
+void entropyToSeed(word256 entropy) {
     SHA256_CTX context;
     uint8_t digest[SHA256_DIGEST_LENGTH];
     SHA256_Init(&context);
-	SHA256_Update(&context, (uint8_t*)(entropy + 2), sizeof(uint64_t)*2);
+	SHA256_Update(&context, (uint8_t*)(entropy + 16), 16);
 	SHA256_Final(digest, &context);
     shl256(entropy, 4);
     entropy[31] += digest[0] >> 4;
-    return (int)(digest[0] & 0xff);
+}
+
+char** seedToMneumonic (word256 seed) {
+    word256 buf;
+    word256 mask;
+    word256 _seed;
+    memcpy(_seed, seed, 32);
+    shl256(mask, 256);
+    mask[31] = 255;
+    mask[30] = 7;
+    int idx;
+    char** mneumonic = malloc(sizeof(char*) * 12);
+    for (int i = 0; i < 12; i++) {
+        and256(_seed, mask, buf);
+        idx = (buf[30] << 8) + buf[31];
+        mneumonic[i] = (char*) mneumonics[idx];
+        shr256(_seed, 11);
+    }
+    return mneumonic;
 }
